c82b7474b41631e94ab057def385e139715bb1c0
edjunma <edjunma@gmail.com>
2021-02-26 23:47:09 -0500

    working on coinlist

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,17 @@
-import Head from 'next/head'
-import SearchBar from '../components/SearchBar'
-import styles from '../styles/Home.module.css'
+import Coins from '../components/Coins';
+import SearchBar from '../components/SearchBar';
+
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
-        <title>Create Next App</title>
+        <title>Crypto Tracker</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <SearchBar type='text' placeholder='Search'/>
+      <Coins />
     </div>
   )
 }
