59fb5a020102838e5f85fe65f6a7252e115c59b6
edjunma <edjunma@gmail.com>
2021-03-01 23:47:45 -0500

    add api request

--- pages/index.js
+++ pages/index.js
@@ -15,3 +15,15 @@ export default function Home() {
     </div>
   )
 }
+
+export const getServerSideProps = async () => {
+  const res = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false')
+
+  const filteredCoins = await res.json()
+
+  return {
+    props: {
+      filteredCoins
+    }
+  }
+}
