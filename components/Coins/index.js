6a95204c0649c4c06c416ea8db939428f624a106
edjunma <edjunma@gmail.com>
2021-03-03 01:13:12 -0500

    working on coin page setup

--- components/Coins/index.js
+++ components/Coins/index.js
@@ -5,6 +5,21 @@ const Coins = ({ name, price, symbol, marketcap, volume, image, priceChange, id 
         <div className="coin">
           <img src={image} alt={name} className="coin_img"/>
           <h1 className="coin_h1">{name}</h1>
+          <p className="coin_symbol">{symbol}</p>
+        </div>
+        <div className="coin_data">
+          <p className="coin_price">${price}</p>
+          <p className="coin_volume">${volume.toLocaleString()}</p>
+
+          {priceChange < 0 ? (
+            <p className="coin_percent red">
+              {priceChange.toFixed(2)}%
+            </p>
+          ) : (
+            <p className="coin_percent green">
+              {priceChange.toFixed(2)}%
+            </p>
+          )}
         </div>
       </div>
     </div>
